5482183d83ff38e0015aa0fad753ddc9edcae964
ian31174117 <ian31174117@gmail.com>
2025-05-22 21:00:32 +0800

    [feat] Add pytest for functions in main.py, fix bug of orderitem mismatch

--- backend/order_service/models.py
+++ backend/order_service/models.py
@@ -5,12 +5,14 @@ from datetime import datetime
 
 Base = declarative_base()
 
+"""
 order_items = Table('order_items', Base.metadata,
     Column('order_id', Integer, ForeignKey('orders.id'), primary_key=True),
     Column('menu_item_id', Integer, ForeignKey('menu_items.id'), primary_key=True),
     Column('quantity', Integer),
     Column('unit_price', Float)
 )
+"""
 
 class Order(Base):
     __tablename__ = "orders"
@@ -23,7 +25,8 @@ class Order(Base):
     payment_method = Column(String)  # cash, credit
     payment_status = Column(String)  # paid, unpaid
     
-    items = relationship("MenuItem", secondary=order_items, back_populates="orders")
+    #items = relationship("MenuItem", secondary=order_items, back_populates="orders")
+    order_items = relationship("OrderItem", back_populates="order")
 
 class MenuItem(Base):
     __tablename__ = "menu_items"
@@ -37,4 +40,16 @@ class MenuItem(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
-    orders = relationship("Order", secondary=order_items, back_populates="items") 
+    #orders = relationship("Order", secondary=order_items, back_populates="items") 
+    order_items = relationship("OrderItem", back_populates="menu_item")
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    order_id = Column(Integer, ForeignKey("orders.id"), primary_key=True)
+    menu_item_id = Column(Integer, ForeignKey("menu_items.id"), primary_key=True)
+    quantity = Column(Integer)
+    unit_price = Column(Float)
+
+    order = relationship("Order", back_populates="order_items")
+    menu_item = relationship("MenuItem", back_populates="order_items")
